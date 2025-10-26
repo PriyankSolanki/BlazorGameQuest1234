afd063d62eb1c0c63efee44cb3ea381310c01a47
UNUSkh <younes-ayoub.khoya@etu.eilco.univ-littoral.fr>
2025-10-26 23:29:36 +0100

    Ajout des fonctionnalies de la version 2

---
+++ GameServices/Controllers/CharactersController.cs
@@ -0,0 +1,72 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AuthenticationServices;
+using SharedModels;
+
+namespace GameServices.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CharactersController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CharactersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Charactere>> GetAll()
+        {
+            return Ok(_context.Characters.ToList());
+        }
+
+
+        [HttpGet("{id}")]
+        public ActionResult<Charactere> GetById(int id)
+        {
+            var c = _context.Characters.FirstOrDefault(x => x.Id == id);
+            if (c == null) return NotFound();
+            return Ok(c);
+        }
+
+
+        // POST: api/characters
+        
+        [HttpPost]
+        public ActionResult<Charactere> CreateBase(Charactere chara)
+        {
+            _context.Characters.Add(chara); 
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetById), new { id = chara.Id }, chara);
+        }
+
+        // PUT: api/characters/5
+        [HttpPut("{id:int}")]
+        public IActionResult Update(int id, Charactere chara)
+        {
+            var existing = _context.Characters.FirstOrDefault(x => x.Id == id);
+            if (existing == null) return NotFound();
+
+            existing.name = chara.name;
+            existing.PV = chara.PV;
+            existing.ATQ = chara.ATQ;
+
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        // DELETE: api/characters/5
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete(int id)
+        {
+            var existing = _context.Characters.FirstOrDefault(x => x.Id == id);
+            if (existing == null) return NotFound();
+
+            _context.Characters.Remove(existing);
+            _context.SaveChanges();
+            return NoContent();
+        }
+    }
+}
